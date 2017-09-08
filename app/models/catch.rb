c3bbc2b403716bdce345bc3439dcaa8cb2d96078
Matias Jara <mjara.figueroa@gmail.com>
2017-09-08 11:29:32 -0300

    tabla intermedia catch, gem railroady

---
+++ app/models/catch.rb
@@ -0,0 +1,4 @@
+class Catch < ApplicationRecord
+  belongs_to :pokemon
+  belongs_to :user
+end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,4 @@
 class Pokemon < ApplicationRecord
+  has_many :catches
+  has_many :users, through: :catches
 end
